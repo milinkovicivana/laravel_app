fd473c6bb6e3328f2d84018455878cc9fa9d0647
ivana <ivanamilinkovic6@gmail.com>
2019-02-18 00:23:17 +0100

    Odradjen ispis korisnika, forma za unos korisnika i validacija, pre dodavanja slika

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,1 +1,44 @@
-<h1>Create</h1>
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+<h1>Create users</h1>
+
+{!! Form::open(['action'=>'AdminUsersController@store']) !!}
+
+	<div class="form-group">
+
+		{!! Form::label('name', 'Name:') !!}
+		{!! Form::text('name', null, ['class'=>'form-control']) !!}
+
+		{!! Form::label('email', 'Email:') !!}
+		{!! Form::email('email', null, ['class'=>'form-control']) !!}
+
+        {!! Form::label('password', 'Password:') !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+
+        {!! Form::label('role_id', 'Role:') !!}
+        {!! Form::select('role_id', array('' => 'Choose') + $roles, null, ['class'=>'form-control']) !!}
+
+        {!! Form::label('is_active', 'Active:') !!}
+        {!! Form::select('is_active', array(1 => 'Yes', 0 => 'No'), 0, ['class'=>'form-control']) !!}
+
+
+
+	</div>
+	<div class="form-group">
+
+	    {!! Form::submit('Create', ['class'=>'btn btn-primary']) !!}
+
+	</div>
+
+{!! Form::close() !!}
+
+
+@include('includes.formError')
+
+@stop
+
+
