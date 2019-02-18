010cfcdec9c3b460b7e789340d45f5969009e457
ivana <ivanamilinkovic6@gmail.com>
2019-02-18 19:17:51 +0100

    Odradjen admin middleware, neprijavljen korisnik niti iko ko nije admin nema pristup admin delu

--- app/User.php
+++ app/User.php
@@ -33,4 +33,14 @@ class User extends Authenticatable
 
         return $this->belongsTo('App\Photo');
     }
+
+    public function isAdmin(){
+
+        if($this->role->name == 'administrator'){
+
+            return true;
+        }
+
+        return false;
+    }
 }
