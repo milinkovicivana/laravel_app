787152c5e90bb39b258a6fbb02ac6f96bd4036d5
ivana <ivanamilinkovic6@gmail.com>
2019-02-21 14:15:54 +0100

    Odradjeno sve za kategorije

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,16 +20,18 @@ Route::auth();
 Route::get('/home', 'HomeController@index');
 
 
-
 Route::get('/admin', function (){
 
     return view('admin.index');
 });
+
 
 Route::group(['middleware'=>'admin'], function(){
 
     Route::resource('/admin/users', 'AdminUsersController');
 
     Route::resource('/admin/posts', 'AdminPostsController');
+
+    Route::resource('/admin/categories', 'AdminCategoriesController');
 
 });
---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+    @if(Session::has('created_category'))
+
+        <p class="bg-success">{{session('created_category')}}</p>
+
+    @elseif(Session::has('updated_category'))
+
+        <p class="bg-success">{{session('updated_category')}}</p>
+
+    @elseif(Session::has('deleted_category'))
+
+        <p class="bg-danger">{{session('deleted_category')}}</p>
+
+    @endif
+
+  <h1>Categories</h1>
+
+
+  <div class="col-sm-6">
+
+      {!! Form::open(['action'=>'AdminCategoriesController@store']) !!}
+
+      <div class="form-group">
+
+          {!! Form::label('name', 'Name:') !!}
+          {!! Form::text('name', null, ['class'=>'form-control']) !!}
+
+      </div>
+      <div class="form-group">
+
+          {!! Form::submit('Create', ['class'=>'btn btn-primary']) !!}
+
+      </div>
+
+      {!! Form::close() !!}
+
+
+  </div>
+
+  <div class="col-sm-6">
+
+      <table class="table">
+          <thead>
+          <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Created</th>
+              <th>Updated</th>
+          </tr>
+          </thead>
+          <tbody>
+
+          @if($categories)
+
+              @foreach($categories as $category)
+
+                  <tr>
+                      <td>{{$category->id}}</td>
+                      <td><a href="{{route('admin.categories.edit', $category->id)}}">{{$category->name}}</a></td>
+                      <td>{{$category->created_at != null? $category->created_at->diffForHumans() : ""}}</td>
+                      <td>{{$category->updated_at != null? $category->updated_at->diffForHumans() : ""}}</td>
+                  </tr>
+
+              @endforeach
+
+          @endif
+
+          </tbody>
+      </table>
+
+  </div>
+
+
+
+
+
+@stop
